39cfe50f64eb3492b82ee40bd8cded5c50948ce3
stas <stas.v.nebyshinets@gmail.com>
2021-02-15 23:05:41 +0300

    feature: implement towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix == undefined){ return [] }
+  let result = [];
+  matrix.forEach( (row, index) => {
+    if(index%2 == 1){
+      row.reverse();
+    }
+    row.forEach( value => result.push(value) );
+  })
+  return result;
 }
